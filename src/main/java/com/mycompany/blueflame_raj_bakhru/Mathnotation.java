4b6c31ffca5d8e36cd1d2c1505de3c56f316331e
AyanAutoomation <sangupta.ayan90@gmail.com>
2024-10-23 13:58:47 +0530

    math

--- src/main/java/com/mycompany/blueflame_raj_bakhru/Mathnotation.java
+++ src/main/java/com/mycompany/blueflame_raj_bakhru/Mathnotation.java
@@ -1,48 +1,31 @@
 
 package com.mycompany.blueflame_raj_bakhru;
-
+import com.bfagent.pom.Autocompletexpaths;
+import com.bfagent.pom.Mathnotations_paths;
 import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class Mathnotation {
     
     WebDriver d;
-    
 
-
-
-public Mathnotation(WebDriver d){
+    public Mathnotation(WebDriver d){
 
 this.d = d;
-
-
 }
-
-
 public void checkmathnotation() throws InterruptedException{
 
-  String Editor_Box = "//p[@data-placeholder = 'Ask me a question' ]";
-  
-  String Send ="//i[@class='ico1']";
-  String Export_button = "//div[@class='MuiStack-root repl_share css-u4p24i']//button[@type='button']";
-  
   WebDriverWait w = new WebDriverWait(d,Duration.ofSeconds(100));
-       
-  w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Editor_Box)));
-  d.findElement(By.xpath(Editor_Box)).sendKeys("@chatgpt show all arccosine functions");
+  Autocompletexpaths path = new Autocompletexpaths(d);
+  Mathnotations_paths mpath = new Mathnotations_paths(d);
+  
+  w.until(ExpectedConditions.visibilityOf(path.Editor_Box()));
+  path.Editor_Box().sendKeys("@chatgpt show all arccosine functions");
   Thread.sleep(1000); 
-  w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Send)));      
-  d.findElement(By.xpath(Send)).click();
-      
-  w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Export_button)));
-
-
-
-
-}
-
-}
+  w.until(ExpectedConditions.visibilityOf(mpath.Send()));      
+  mpath.Send().click();
+  w.until(ExpectedConditions.visibilityOf(mpath.Export_button()));
+}}

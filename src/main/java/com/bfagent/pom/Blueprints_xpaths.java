2a84025c16d9a6bdf5c6824f766524a460ee0150
AyanAutoomation <sangupta.ayan90@gmail.com>
2024-12-05 19:46:09 +0530

    Blueprint

---
+++ src/main/java/com/bfagent/pom/Blueprints_xpaths.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bfagent.pom;
+
+/**
+ *
+ * @author webskitters
+ */
+public class Blueprints_xpaths {
+    
+}
